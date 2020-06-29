392c748ca6b9b382feeeb49a1835742108e577cb
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2020-06-30 00:46:52 +0800

    google social login

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from account import views
-
+from django.conf.urls import include
 
 app_name = 'account'
 urlpatterns = [
    path('register/', views.register, name='register'), 
    path('login/', views.login, name='login'),
    path('logout/', views.logout, name='logout'),
+   path('', include('social_django.urls', namespace='social')),
 ]
