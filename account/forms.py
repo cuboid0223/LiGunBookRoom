aa6dc57f539d3589797e0568de88153c2de030d8
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2020-07-06 20:00:15 +0800

    google-login and homepage CSS (RWD)

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from account.models import User
 
+
+from django.core.files.images import get_image_dimensions#使用者頭像
+
+
 class UserForm(forms.ModelForm):
     username = forms.CharField(label='帳號')
     password = forms.CharField(label='密碼', widget=forms.PasswordInput)
@@ -8,10 +12,10 @@ class UserForm(forms.ModelForm):
     fullname = forms.CharField(label='姓名', max_length=128)
     website = forms.URLField(label='個人網址', max_length=128, required=False)
     address = forms.CharField(label='住址', max_length=128, required=False)# required=False 表示非必填選項欄
-
+    avater = forms.ImageField(label='頭貼', required=False)  # 使用者頭像
     class Meta:
         model = User
-        fields = ['username', 'password', 'password2', 'fullname', 'website', 'address']
+        fields = '__all__'
     
     def clean_password2(self):
         password = self.cleaned_data.get('password')
@@ -25,3 +29,39 @@ class UserForm(forms.ModelForm):
         user.set_password(user.password)
         user.save()
         return user
+
+    def clean_avatar(self):  # 使用者頭像
+        avatar = self.cleaned_data['avatar']
+
+        try:
+            w, h = get_image_dimensions(avatar)
+
+            #validate dimensions
+            max_width = max_height = 100
+            if w > max_width or h > max_height:
+                raise forms.ValidationError(
+                    u'Please use an image that is '
+                    '%s x %s pixels or smaller.' % (max_width, max_height))
+
+            #validate content type
+            main, sub = avatar.content_type.split('/')
+            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+                raise forms.ValidationError(u'Please use a JPEG, '
+                                            'GIF or PNG image.')
+
+            #validate file size
+            if len(avatar) > (20 * 1024):
+                raise forms.ValidationError(
+                    u'Avatar file size may not exceed 20k.')
+
+        except AttributeError:
+            """
+            Handles case when we are updating the user profile
+            and do not supply a new avatar
+            """
+            pass
+
+        return avatar
+
+
+    
--- account/models.py
+++ account/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings # 使用者頭像
+imgUser = settings.AUTH_USER_MODEL  # 使用者頭像
 # Create your models here.
 
 class User(AbstractUser):
     fullName = models.CharField(max_length=128)
     website = models.URLField(blank=True, null=True)
     address = models.CharField(max_length=128, blank=True, null=True)
+    
 
     def __str__(self):
         return self.fullName+'(' + self.username + ')'
+
+
+
+    
