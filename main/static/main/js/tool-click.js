305c95895baf62d1400c6227a8f7158077d6af8e
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2020-07-10 23:52:27 +0800

    article page CSS

--- main/static/main/js/tool-click.js
+++ main/static/main/js/tool-click.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
     $('.tool').on('click', function () {
         $('.update-box ').css('opacity','1');
-        $(".update-box").toggle(2000);
-        
+        $('.update-box ').css('position', 'unset');
+        // $(".update-box").toggle(2000);    
     });
 
     // function slideToggle() {
     //     $(".update-box").slideToggle(2000); // 窗簾效果的切換,點一下收,點一下開,參數可以無,參數說明同上
     // }
     // slideToggle()
+
+    // $('.tool').toggle(
+    //     function () { $(".update-box").css('opacity', '1'); },
+    //     function () { $(".update-box").css('opacity', '0'); }
+    // );
 });
 
