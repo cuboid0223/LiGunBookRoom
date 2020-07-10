8efdd583cf6533d6e92a0a34a340e2dfefe83e3c
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2020-07-11 00:37:07 +0800

    account models problem

--- account/forms.py
+++ account/forms.py
@@ -8,12 +8,12 @@ class UserForm(forms.ModelForm):
     username = forms.CharField(label='帳號', widget=forms.TextInput(attrs={'class': 'userForm'}))
     password = forms.CharField(label='密碼', widget=forms.PasswordInput and forms.TextInput(attrs={'class': 'userForm'}))
     password2 = forms.CharField(label='確認密碼', widget=forms.PasswordInput and forms.TextInput(attrs={'class': 'userForm'}))
-    fullname = forms.CharField(label='暱稱', max_length=8, widget=forms.TextInput(attrs={'class': 'userForm'}))
+    fullName = forms.CharField(label='暱稱', max_length=8, widget=forms.TextInput(attrs={'class': 'userForm'}))
     website = forms.URLField(label='個人網址', max_length=128, required=False, widget=forms.TextInput(attrs={'class': 'userForm'}))
     address = forms.CharField(label='住址', max_length=128, required=False, widget=forms.TextInput(attrs={'class': 'userForm'}))# required=False 表示非必填選項欄
     class Meta:
         model = User
-        fields = ['username','password','password2','fullname']
+        fields = ['username','password','password2','fullName']
         widgets = {
             # 加入css 配合 widget=forms.TextInput(attrs={'class': 'userForm'})
             'userForm': forms.TextInput(attrs={'class': 'userForm'}),
